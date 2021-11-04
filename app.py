fb0ff25ac05b9838857faf3991098be1e0f290e4
smko yazdanpanah <81965477+smkoyazdanpanah@users.noreply.github.com>
2021-11-04 16:47:01 +0330

    Update app.py
    Remove Banners and Consider PEP8

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
+# Importing Self-made Modules
+from controller import control_socket as cs
+
 # Importing argparse
 import argparse
+
 # Avoiding Pyc Files
 import sys
 sys.dont_write_bytecode = True
 
-# Importing Text Formatting Files
-from pyfiglet import figlet_format
-from termcolor import colored
 
-# Importing Self-made Modules
-from controller import control_socket as cs
-
-if __name__ == "__main__":
-    print(colored(figlet_format("DNS Sample"),"red"))
-    print(colored("\t\t---Made By iHapiW---","red"))
+def main():
     # Create the parser
     parser = argparse.ArgumentParser()
+    
     # Add the arguments
-    parser.add_argument("--DNSServer", type=str)
-    parser.add_argument("Hostname", type=str)
+    parser.add_argument("--dns-server", type=str)
+    parser.add_argument("hostname", type=str)
+    
     # Execute the parse_args() method
     args = parser.parse_args()
-    DNSServer = args.DNSServer if args.DNSServer else "8.8.8.8"
-    Hostname = args.Hostname
-    cs.get_ip(Hostname,DNSServer)
+    
+    dns_server = args.dns_server if args.dns_server else "8.8.8.8"
+    hostname = args.hostname
+    
+    cs.get_ip(hostname, dns_server)
+
+
+if __name__ == "__main__":
+    main()
