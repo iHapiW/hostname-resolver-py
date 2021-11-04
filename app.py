344df8b0af07cc27ccbd508395416b498a374eee
smko yazdanpanah <81965477+smkoyazdanpanah@users.noreply.github.com>
2021-11-04 13:29:33 +0330

    Command Line Interface
    Building Command Line Interface With argparse Library

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
+# Importing argparse
+import argparse
 # Avoiding Pyc Files
 import sys
 sys.dont_write_bytecode = True
 
-#Importing Text Formatting Files
+# Importing Text Formatting Files
 from pyfiglet import figlet_format
 from termcolor import colored
 
-#Importing Self-made Modules
-from model.stdio import getinp
+# Importing Self-made Modules
 from controller import control_socket as cs
 
 if __name__ == "__main__":
     print(colored(figlet_format("DNS Sample"),"red"))
     print(colored("\t\t---Made By iHapiW---","red"))
-    DNSServer = getinp("Enter DNS Server (default 8.8.8.8): ")
-    DNSServer = DNSServer if DNSServer != "" else "8.8.8.8"
-    while True:
-        Hostname = getinp("Enter Hostname: ")
-        cs.get_ip(Hostname,DNSServer)
+    # Create the parser
+    parser = argparse.ArgumentParser()
+    # Add the arguments
+    parser.add_argument("--DNSServer", type=str)
+    parser.add_argument("Hostname", type=str)
+    # Execute the parse_args() method
+    args = parser.parse_args()
+    DNSServer = args.DNSServer if args.DNSServer else "8.8.8.8"
+    Hostname = args.Hostname
+    cs.get_ip(Hostname,DNSServer)
