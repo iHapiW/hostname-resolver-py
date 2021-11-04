66c585ce2f5f459c764f9cfe1390eb6138da7c33
iHapiW <iHapiW@gmail.com>
2021-11-04 16:31:49 +0330

    Update UI & Add Parser

--- controller/control_socket.py
+++ controller/control_socket.py
@@ -1,7 +1,7 @@
-import socket
-import struct
+from socket import gaierror
+from struct import error
 
-from model.stdio import error, success, warning
+from model.stdio import error, success
 from model import connection as c
 
 #Connection Model Handler
@@ -11,14 +11,14 @@ def get_ip(hostname : str, dnsserver : str):
         packet = connection.make_packet(hostname.strip())
         result = connection.iopacket(packet)
         answer = connection.analyze_packet(packet,result)
-        success(f"""{{
-    ---DNS Answer Recieved----
+        success("Host Found",f"""{{
     Type = {answer.type}
     Class = {answer.dns_class}
     TTL = {answer.ttl}
     Address = {answer.address}
-}}""")
-    except socket.gaierror:
-        error("Cannot Access DNS Server!")
-    except struct.error:
-        warning("Invalid Host")
+}}
+""")
+    except gaierror:
+        error("Invalid DNS")
+    except error:
+        error("Host Not Found")
